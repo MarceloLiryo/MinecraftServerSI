6dad70998bb100bfee4eddff683a4fc8ff79736e
Marcelo Líryo <marceloliryo@gmail.com>
2026-09-18 14:40:14 -0300

    Feat: Scripts de aviso de mais mortes e configs

--- start.bat
+++ start.bat
@@ -1,5 +1,6 @@
 @echo off
-
-java -Xms4096M -Xmx4096M -jar server.jar
-
-exit
+:loop
+"C:\Program Files\Eclipse Adoptium\jdk-25.0.4.101-hotspot\bin\java.exe" -Djavax.net.ssl.trustStore=NONE -Djavax.net.ssl.trustStoreType=WINDOWS-ROOT -Xms5120M -Xmx5120M -jar server.jar
+echo Servidor parou. Reiniciando em 10 segundos... (Ctrl+C agora para cancelar)
+timeout /t 15
+goto loop
